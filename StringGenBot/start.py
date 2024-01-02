1a28a811a15362b3bacfeda7e43c73ca538f96fc
bdr <121260038+bdr20072@users.noreply.github.com>
2024-01-02 18:16:35 +0300

    start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,21 +12,21 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""𝐇𝐞𝐲 {msg.from_user.mention}🍷,
+        text=f"""هلو {msg.from_user.mention}🍷,
 
 ɪ ᴀᴍ {me2},
 ᴛʀᴜsᴛᴇᴅ 𝗦𝗧𝗥𝗜𝗡𝗚 𝗚𝗥𝗡𝗘𝗥𝗔𝗧𝗢𝗥 ʙᴏᴛ.
 ғᴜʟʟʏ sᴀғᴇ & sᴇᴄᴜʀᴇ.
 ɴᴏ ᴀɴʏ ᴇʀʀᴏʀ
 
-𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝗡𝗲𝗶𝗺𝗮𝗻](tg://user?id={OWNER_ID}) !""",
+𝐌𝐚𝐝𝐞 𝐁𝐲  : [Badr](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="⚡𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆⚡", callback_data="generate")
+                    InlineKeyboardButton(text="⚡بدء الجلسه⚡", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ❣️", url="https://t.me/Neiman_X_Support"),
+                    InlineKeyboardButton("❣️ مساعده ❣️", url="https://t.me/Neiman_X_Support"),
                     InlineKeyboardButton("🥀 𝐎𝐟𝐟𝐢𝐜𝐞 🥀", url="https://t.me/DETECTED_09")
                 ]
             ]
